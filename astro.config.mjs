f6641a6418bec09c8bd19c3a57a1986ad5470457
GDayronHP <geometryhuayra@gmail.com>
2024-12-22 18:15:24 -0500

    Prueba N° 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
-export default defineConfig({
-  site: "https://gdayronhp.github.io/Portfolio",
-  base: '/Portfolio',
-  integrations: [tailwind()],
+// @ts-check
+import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+export default defineConfig({
+  site:("https://gdayronhp.github.io/Portfolio"),
+  base: '/Portfolio', // Asegúrate de usar un slash inicial y final
+  integrations: [tailwind()],
 });
